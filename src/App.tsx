1ec9d4d25e43ca6daa0331013bd12c3190d76fc1
keaising <keaising@gmail.com>
2024-02-21 00:58:18 +0800

    function finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,80 @@
 import "./App.css";
-import SrcList from "./Src";
+import { useEffect, useState } from "react";
+import JSZip from "jszip";
+import { saveAs } from "file-saver";
 
 function App() {
-  const divsWithTestId = document.querySelectorAll(
-    '[data-testid="cellInnerDiv"]',
-  );
+  const [imgSrcList, setImgSrcList] = useState<string[]>([]);
 
-  divsWithTestId.forEach((div) => {
-    const img = div.querySelector("img");
+  useEffect(() => {
+    const observer = new MutationObserver((mutationsList) => {
+      const newImgSrcList: string[] = [];
+      mutationsList.forEach((mutation) => {
+        if (mutation.type === "childList") {
+          mutation.addedNodes.forEach((node) => {
+            if (
+              node instanceof HTMLImageElement &&
+              node.src.startsWith("https://pbs.twimg.com/media/")
+            ) {
+              newImgSrcList.push(node.src);
+            }
+          });
+        }
+      });
+      setImgSrcList((pre) => {
+        newImgSrcList.forEach((s) => {
+          if (!pre.includes(s)) {
+            pre.push(s);
+          }
+        });
+        return [...pre];
+      });
+    });
 
-    if (img) {
-      console.log(img.src); // 这里可以对src进行任何你需要的处理
-    }
-  });
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [imgSrcList]);
+
+  async function download() {
+    let ps = imgSrcList.map((url, _) => downloadURL(url));
+    console.log(ps);
+    await Promise.all(ps).then((values) => {
+      // console.log(values);
+      var zip = new JSZip();
+      var img = zip.folder("media");
+
+      values.forEach((data) => {
+        img!.file(`${data.name}.${data.format}`, data.data);
+      });
+      zip.generateAsync({ type: "blob" }).then(function (content) {
+        // see FileSaver.js
+        saveAs(content, `${window.location.pathname.split("/")[1]}.zip`);
+      });
+    });
+  }
+
+  async function downloadURL(urlStr: string) {
+    // https://pbs.twimg.com/media/GGw-LBGasAAtoMP?format=jpg&name=4096x4096
+    let url = new URL(urlStr);
+    let pathnames = url.pathname.split("/");
+    let name = pathnames[pathnames.length - 1];
+    let format = url.searchParams.get("format");
+    url.searchParams.set("name", "large");
+    return fetch(url.toString()).then((resp) => {
+      return {
+        name: name,
+        format: format,
+        data: resp.blob(),
+      };
+    });
+  }
+
   return (
     <div className="App fixed">
-      <h1>Vite + React</h1>
-      <SrcList />
+      <button onClick={download}>download media</button>
     </div>
   );
 }
